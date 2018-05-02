52dad72f9685cf578ee0b24ee6128cb672533abe
Emily Kolar <emkolardev@gmail.com>
2018-05-01 20:44:17 -0500

    replace transact with data

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import uuid from 'uuid4';
 
-import Blockchain from './blockchain';
+import Blockchain from './blockchain-1';
 
 const blockChain = new Blockchain();
 const nodeId = uuid().toString().replace('-', '');
 const app = express();
+
+const args = process.argv;
+const port = args[2] || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -18,7 +21,7 @@ app.get('/mine', (req, res) => {
   const lastProof = lastBlock.proof;
   const proof = Blockchain.proofOfWork(lastProof);
 
-  blockChain.newTransaction(0, nodeId, 1);
+  blockChain.newDataTransaction(0, nodeId, 1);
 
   const previousHash = Blockchain.hash(lastBlock);
   const block = blockChain.newBlock(proof, previousHash);
@@ -26,7 +29,7 @@ app.get('/mine', (req, res) => {
   const response = {
     message: 'New block forged',
     index: block.index,
-    transactions: block.transactions,
+    data: block.data,
     proof: block.proof,
     previousHash: block.previousHash,
   };
@@ -35,14 +38,14 @@ app.get('/mine', (req, res) => {
 });
 
 app.post('/transactions', (req, res) => {
-  const { sender, recipient, amount } = req.body;
+  const { sender, recipient, data } = req.body;
 
-  if (!sender || !recipient || !amount) {
+  if (!sender || !recipient || !data) {
     return res.sendStatus(400);
   }
 
-  const index = blockChain.newTransaction(sender, recipient, amount);
-  const response = { message: `Transaction added to block ${index}` };
+  const index = blockChain.newDataTransaction(sender, recipient, data);
+  const response = { message: `Data transaction added to block ${index}` };
 
   return res.status(200).json(response);
 });
@@ -56,4 +59,31 @@ app.get('/chain', (req, res) => {
   return res.status(200).json(response);
 });
 
-app.listen(5000, () => console.log('Listening on port 5000'));
+app.post('/nodes/register', (req, res) => {
+  const { nodes } = req.body;
+
+  if (!nodes) return res.sendStatus(400);
+
+  const nodeArray = JSON.parse(nodes);
+  nodeArray.forEach(node => blockChain.registerNode(node));
+
+  const response = {
+    message: 'New nodes added',
+    totalNodes: blockChain.nodes.entries(),
+  };
+
+  return res.status(200).json(response);
+});
+
+app.get('/nodes/resolve', async (req, res) => {
+  const response = { message: '', newChain: blockChain.chain };
+  const replaced = await blockChain.resolveConflicts();
+  if (replaced) {
+    response.message = 'Chain replaced';
+  } else {
+    response.message = 'Chain is authoritative';
+  }
+  return res.status(200).json(response);
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
