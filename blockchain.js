cbb1f719d02001cb55661163d977de76287ab412
Emily Kolar <emkolardev@gmail.com>
2018-05-01 20:44:36 -0500

    start alternate

--- blockchain.js
+++ blockchain.js
@@ -1,90 +1,76 @@
 import { sha256 } from 'js-sha256';
 
-function forgeBlock(index, transactions, proof, previousHash) {
-  return {
-    index,
-    timestamp: Date.now(),
-    transactions,
-    proof,
-    previousHash,
-  };
-}
-
-function blockString(block) {
-  const dict = {};
-  Object.keys(block).sort().forEach(key => dict[key] = block[key]);
-  return JSON.stringify(dict).toString();
-}
-
 function getHashHex(string) {
   let hash = sha256.create();
   hash.update(string);
   return hash.hex();
 }
 
-class Blockchain {
-  constructor() {
-    this.chain = [];
-    this.currentTransactions = [];
-
-    this.newBlock(100, 1);
+class Block {
+  constructor(timestamp, data) {
+    this.index = 0;
+    this.timestamp = timestamp;
+    this.data = data;
+    this.previousHash = "0";
+    this.hash = this.calculateHash();
+    this.nonce = 0;
   }
 
-  static validProof(lastProof, proof) {
-    const guess = `${lastProof}${proof}`.toString();
-    const guessHash = getHashHex(guess);
-
-    return guessHash.substring(0, 4) === '0000';
-  }
-
-  /*
-   * Proof of Work:
-   * Find a number p' such that hash(pp') contains leading 4 zeroes, where p is
-   * the previous proof, and p' is the new proof
-  */
-  static proofOfWork(lastProof) {
-    let proof = 0;
-
-    while (!Blockchain.validProof(lastProof, proof)) {
-      proof += 1;
-    }
-
-    return proof;
-  }
-
-  newBlock(proof, previousHash = null) {
-    const block = forgeBlock(
-      this.chain.length + 1,
-      this.currentTransactions,
-      proof,
-      previousHash || Blockchain.hash(this.lastBlock),
-    );
-
-    this.currentTransactions = [];
-    this.chain.push(block);
-
-    return block;
-  }
-
-  newTransaction(sender, recipient, amount) {
-    this.currentTransactions.push({
-      sender,
-      recipient,
-      amount,
-    });
-
-    return this.lastBlock.index + 1;
-  }
-
-  static hash(block) {
-    const str = blockString(block);
-
+  calculateHash() {
+    const str =
+      `${this.index}${this.previousHash}${this.timestamp}${this.data}${this.nonce}`
+      .toString();
     return getHashHex(str);
   }
 
-  get lastBlock() {
-    return this.chain[this.chain.length - 1];
+  mineBlock(difficulty) {
+
   }
 }
 
-export default Blockchain;
+class Blockchain{
+  constructor() {
+    this.chain = [this.createGenesis()];
+  }
+
+  createGenesis() {
+    const date = (new Date()).toLocaleDateString();
+    return new Block(0, date, 'Genesis Block', '0');
+  }
+
+  latestBlock() {
+    return this.chain[this.chain.length - 1];
+  }
+
+  addBlock(newBlock){
+    newBlock.previousHash = this.latestBlock().hash;
+    newBlock.hash = newBlock.calculateHash();
+    this.chain.push(newBlock);
+  }
+
+  checkValid() {
+    for (let i = 1; i < this.chain.length; i++) {
+      const currentBlock = this.chain[i];
+      const previousBlock = this.chain[i - 1];
+
+      if (currentBlock.hash !== currentBlock.calculateHash()) {
+        return false;
+      }
+
+      if (currentBlock.previousHash !== previousBlock.hash) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+}
+
+let chain = new Blockchain();
+chain.addBlock(new Block("12/25/2017", {amount: 5}));
+chain.addBlock(new Block("12/26/2017", {amount: 10}));
+
+console.log(JSON.stringify(chain, null, 4));
+console.log("Is blockchain valid? " + chain.checkValid());
+
+
